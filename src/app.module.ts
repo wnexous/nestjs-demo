628c2d41c97f45b2d56f2cf113d314e02c04aab5
wnexous <andreneexus@hotmail.com>
2023-06-06 03:36:46 +0000

    add teste-nuclear module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TesteNuclearModule } from './app/teste-nuclear/teste-nuclear.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    TesteNuclearModule,
   ],
   controllers: [],
   providers: [],
